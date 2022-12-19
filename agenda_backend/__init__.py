bf90d1a9b85d0f7e5377a0efc6ecb0c1e7f35542
Jacopo Schiavon <jacoposchiavon21@gmail.com>
2022-12-19 10:44:02 +0100

    fixed error with reservations

--- agenda_backend/__init__.py
+++ agenda_backend/__init__.py
@@ -4,6 +4,7 @@ from agenda_backend.database import db
 from agenda_backend.blueprints.utente import utente
 from agenda_backend.blueprints.tavolo import tavolo
 from agenda_backend.blueprints.cliente import cliente
+from agenda_backend.blueprints.prenotazione import prenotazione
 
 
 def create_app(*args, **kwargs):
@@ -13,5 +14,6 @@ def create_app(*args, **kwargs):
     app.register_blueprint(utente)
     app.register_blueprint(tavolo)
     app.register_blueprint(cliente)
+    app.register_blueprint(prenotazione)
     db.init_app(app)
     return app
--- agenda_backend/models/prenotazione.py
+++ agenda_backend/models/prenotazione.py
@@ -8,20 +8,20 @@ class Prenotazione(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     id_Ristorante = db.Column(db.Integer, nullable=False)
-    DataOra = db.Column(db.datetime, nullable=False)
+    DataOra = db.Column(db.DateTime, nullable=False)
     Tavolo= db.Column(db.Integer, nullable=False)
     Numero_Posti = db.Column(db.Integer, nullable=False)
     id_Cliente = db.Column(db.Integer, nullable=False)
     Note = db.Column(db.String, nullable=True)
     id_User = db.Column(db.Integer, nullable=False)
-    DataOra_Prenotazione = db.Column(db.datetime, nullable=False)
+    DataOra_Prenotazione = db.Column(db.DateTime, nullable=False)
     Flag_Disdetta = db.Column(db.Boolean, nullable=True)        #<------- eventualmente si può fare come stringa si/no
 
 
 class __PrenotazioneSchema(Schema):
     id = fields.Int(dump_only=True)
     id_Ristorante = fields.Int()
-    DataOra = field.DateTime()
+    DataOra = fields.DateTime()
     Tavolo= fields.Int()
     Numero_Posti = fields.Int()
     id_Cliente = fields.Int()
