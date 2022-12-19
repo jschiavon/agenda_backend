d984d256586d674006f47e6314b49682db944403
Jacopo Schiavon <jacoposchiavon21@gmail.com>
2022-12-19 14:22:25 +0100

    solved CORS problem

--- agenda_backend/__init__.py
+++ agenda_backend/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from agenda_backend.database import db
 from agenda_backend.blueprints.utente import utente
@@ -10,6 +11,7 @@ from agenda_backend.blueprints.prenotazione import prenotazione
 def create_app(*args, **kwargs):
     env = kwargs['env']
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object('config.%s' % env)
     app.register_blueprint(utente)
     app.register_blueprint(tavolo)
